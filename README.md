14904ea7f92d3447de915ddd42b78b097fe1f3f6
Ruslan <rvslan@pm.me>
2022-08-07 18:50:34 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# react-native-nft-marketplace
+NFT Marketplace - Modern UI/UX React Native App
+![Coinbase clone](https://github.com/rvslan/react-native-nft-marketplace/blob/main/screenshot.png?raw=true)
+
+## Getting Started
+
+First, run the development server:
+
+```bash
+npm start
---
+++ components/SubInfo.js
@@ -0,0 +1,138 @@
+import { View, Text, Image } from 'react-native';
+import React from 'react';
+
+import { SIZES, FONTS, COLORS, SHADOWS, assets } from '../constants';
+
+const shufflePeople = () => {
+  const newArr = [assets.person02, assets.person03, assets.person04].slice();
+
+  for (let i = newArr.length - 1; i > 0; i--) {
+    const rand = Math.floor(Math.random() * (i + 1));
+    [newArr[i], newArr[rand]] = [newArr[rand], newArr[i]];
+  }
+  return newArr;
+};
+
+export const NFTTitle = ({ title, subTitle, titleSize, subTitleSize }) => {
+  return (
+    <View>
+      <Text
+        style={{
+          fontFamily: FONTS.semiBold,
+          fontSize: titleSize,
+          color: COLORS.primary,
+        }}
+      >
+        {title}
+      </Text>
+      <Text
+        style={{
+          fontFamily: FONTS.regular,
+          fontSize: subTitleSize,
+          color: COLORS.primary,
+        }}
+      >
+        by {subTitle}
+      </Text>
+    </View>
+  );
+};
+
+export const EthPrice = ({ price }) => {
+  return (
+    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+      <Image
+        source={assets.eth}
+        resizeMode='contain'
+        style={{ width: 20, height: 20, marginRight: 2 }}
+      />
+      <Text
+        style={{
+          fontFamily: FONTS.medium,
+          fontSize: SIZES.font,
+          color: COLORS.primary,
+        }}
+      >
+        {price}
+      </Text>
+    </View>
+  );
+};
+
+const ImageCmp = ({ imgUrl, index }) => {
+  return (
+    <Image
+      source={imgUrl}
+      resizeMode='contain'
+      style={{
+        width: 48,
+        height: 48,
+        marginLeft: index === 0 ? 0 : -SIZES.font,
+      }}
+    />
+  );
+};
+
+export const People = () => {
+  return (
+    <View style={{ flexDirection: 'row' }}>
+      {shufflePeople().map((imgUrl, index) => (
+        <ImageCmp imgUrl={imgUrl} index={index} key={`People-${index}`} />
+      ))}
+    </View>
+  );
+};
+
+export const EndDate = () => {
+  return (
+    <View
+      style={{
+        paddingHorizontal: SIZES.font,
+        paddingVertical: SIZES.base,
+        backgroundColor: COLORS.white,
+        borderRadius: SIZES.font,
+        justifyContent: 'center',
+        alignItems: 'center',
+        ...SHADOWS.light,
+        elevation: 1,
+        maxWidth: '50%',
+      }}
+    >
+      <Text
+        style={{
+          fontFamily: FONTS.regular,
+          fontSize: SIZES.small,
+          color: COLORS.primary,
+        }}
+      >
+        Ending in
+      </Text>
+      <Text
+        style={{
+          fontFamily: FONTS.semiBold,
+          fontSize: SIZES.medium,
+          color: COLORS.primary,
+        }}
+      >
+        12h 30m
+      </Text>
+    </View>
+  );
+};
+
+export const SubInfo = () => {
+  return (
+    <View
+      style={{
+        width: '100%',
+        paddingHorizontal: SIZES.font,
+        marginTop: -SIZES.extraLarge,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+      }}
+    >
+      <People />
+      <EndDate />
+    </View>
+  );
+};
